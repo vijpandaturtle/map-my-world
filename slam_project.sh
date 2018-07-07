1339cb6d039b1c61659ccaafb0ee736c8c4d087c
Vijayasri Iyer <thisisvij98@gmail.com>
2018-07-07 14:33:52 +0530

    Added lidar mesh, launch script

---
+++ slam_project.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+xterm -e "source /home/workspace/catkin_ws/devel/setup.bash; roslaunch slam_project world.launch" &
+sleep 5
+xterm -e "source /home/workspace/catkin_ws/devel/setup.bash; roslaunch slam_project slam_project teleop.launch" &
+sleep 5
+xterm -e "source /home/workspace/catkin_ws/devel/setup.bash; roslaunch slam_project mapping.launch" &
+sleep 5
+xterm -e "source /home/workspace/catkin_ws/devel/setup.bash; roslaunch slam_project rviz.launch"
